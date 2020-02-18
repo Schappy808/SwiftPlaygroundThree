0c9841a875591da3fe06b5e7515ec1873aec7807
Schappy808 <59860894+Schappy808@users.noreply.github.com>
2020-02-18 10:18:43 -1000

    Completed parts 1-7

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
 var str = "Hello, playground"
+
+//var fruitNames : [String]
+
+var fruitNames = ["Apple", "Banana", "Grape"]
+
+print(fruitNames)
+
+print("I like to eat \(fruitNames)")
+
+for name in fruitNames
+{
+    print("I like to eat " + name)
+}
+    print(fruitNames[0])
